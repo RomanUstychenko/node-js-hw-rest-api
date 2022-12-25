139d6cd2f80b8d6e0c50e28c309daa919dcb71c8
RomanUstychenko <roman.ustychenko@gmail.com>
2022-12-25 15:10:18 +0200

    create ctrl user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const ctrl = require("../../controllers/auth")
+const { ctrlWrapper } = require("../../helpers");
+const { validate } = require("../../middlewares");
+const {schemas} = require("../../models/user");
+
+router.post("/register", validate(schemas.registerSchema), ctrlWrapper(ctrl.register))
 
 module.exports = router;
