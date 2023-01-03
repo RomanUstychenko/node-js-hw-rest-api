4641c442168df5ceb5b595ee0ac2048aa543832c
RomanUstychenko <roman.ustychenko@gmail.com>
2023-01-03 11:04:25 +0200

    deleted comments files

--- schemas/index.js
+++
@@ -1,7 +0,0 @@
-// const contactSchema = require("./contactSchema");
-// const updateFavoriteScheme = require("./updateFavoriteScheme")
-
-// module.exports = {
-//   contactSchema,
-//   updateFavoriteScheme,
-// };
